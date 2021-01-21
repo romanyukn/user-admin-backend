28c4c69f1614b242c56eca52e888931a570f7c19
Natalia Romaniuk <romanyukn@gmail.com>
2021-01-21 19:28:50 +1100

    add model, post and get

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
-router.get('/', (req, res) => {
-  res.send('Hello world');
-})
+router.get('/users', (req, res) => {
+  User.find({  })
+      .then((data) => {
+          console.log(data);
+          res.json(data);
+      })
+      .catch((error) => {
+          console.log(error);
+      });
+});
+
+router.post('/users', (req, res) => {
+  const data = req.body;
+  const newUser = new User(data);
+  newUser.save((error) => {
+      if (error) {
+          res.status(500).json({ msg: 'Sorry, server errors' });
+          return;
+      }
+      return res.json({
+          msg: 'User has been saved'
+      });
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const app = express();
-const indexRouter = require('./routes/index');
-
 const mongoose = require('mongoose');
+const router = require('./routes/index');
+const app = express();
+
 mongoose.connect("mongodb://localhost/user-admin-backend");
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoos'));
 
-app.use('/', indexRouter);
+app.use(express.json());
+app.use('/api', router);
 
 app.listen(3001);
